2989783a5c060d95b87bdd91e7f5ecd2a2747334
txt2vz <lauriehirsch@gmail.com>
2017-09-07 16:11:55 +0100

    TwitterKeys

---
+++ src/main/groovy/twitter/TwitterKeys.groovy
@@ -0,0 +1,9 @@
+package twitter
+
+class TwitterKeys  {
+
+    final static String consumerKey = ""
+    final static String consumerSecret = ""
+    final static String accessToken = ""
+    final static String accessTokenSecret = ""
+}
