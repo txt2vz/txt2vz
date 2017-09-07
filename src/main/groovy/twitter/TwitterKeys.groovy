efc1f331dde71da7f340d2f526e4ee1ef622917a
txt2vz <lauriehirsch@gmail.com>
2017-09-07 16:25:39 +0100

    lucene

--- src/main/groovy/twitter/TwitterKeys.groovy
+++ src/main/groovy/twitter/TwitterKeys.groovy
@@ -2,8 +2,8 @@ package twitter
 
 class TwitterKeys  {
 
-    final static String consumerKey = ""
-    final static String consumerSecret = ""
-    final static String accessToken = ""
-    final static String accessTokenSecret = ""
+    final static String consumerKey = "u59ay8TtPUn5p9VTHxdFg"
+    final static String consumerSecret = "LOkS2Vl9KTWXH5VMuwhb9RfIcXBXzkvyzzwD0HQtr14"
+    final static String accessToken = "560297710-4UmsMLOILUgIkLx6V5mdH1lbvG8ew8xvQm5YgBhY"
+    final static String accessTokenSecret = "mlwDtpbx9bUKTTk4wpBYVdUagGmBX6bzAYJbktoNM"
 }
