71e9dfcadcacbe3adcdefc6bdbc2c6a137f6bde3
aceslh <lauriehirsch@gmail.com>
2018-03-20 16:53:56 +0000

    R3 spock opec crude test

--- src/test/groovy/processText/R3Spec.groovy
+++ src/test/groovy/processText/R3Spec.groovy
@@ -1,6 +1,17 @@
 package processText
 
 import groovy.json.JsonSlurper
+import lucene.BuildIndex
+import org.apache.lucene.index.Term
+import org.apache.lucene.search.BooleanClause
+import org.apache.lucene.search.BooleanQuery
+import org.apache.lucene.search.Query
+import org.apache.lucene.search.TermQuery
+import org.apache.lucene.store.Directory
+import org.apache.lucene.store.FSDirectory
+
+import java.nio.file.Path
+import java.nio.file.Paths
 
 class R3Spec extends spock.lang.Specification {
 
@@ -31,5 +42,31 @@ class R3Spec extends spock.lang.Specification {
 		json.name == "mln"
 		json.children[0].name == "tonnes"
 		json.children[0].children[0].name == 'wheat'
+	}
+
+	def "R3 index crude opec" (){
+		given:
+		JsonSlurper slurper = new JsonSlurper()
+		// def linuxpath = '/var/lib/jetty/webapps/Indexes/R10CrudeL'
+		String localpath ='Indexes/R3'
+
+		Path indexPath = Paths.get(localpath)
+		Directory directory = FSDirectory.open(indexPath)
+		BooleanQuery.Builder bqb = new BooleanQuery.Builder()
+
+		when:
+		///  catsFreq [01_corn:237, 02_crude:578, 07_ship:286]
+		TermQuery catQ = new TermQuery(new Term(BuildIndex.FIELD_CATEGORY_NAME, '02_crude'));
+		Query q = new TermQuery(new Term(BuildIndex.FIELD_CONTENTS,'opec'))
+		bqb.add(q, BooleanClause.Occur.MUST)
+		BooleanQuery bq = bqb.build()
+
+		WordPairsExtractor wpe = new WordPairsExtractor();
+		def jsonText = wpe.getJSONnetwork(directory, bq)
+		def json = slurper.parseText(jsonText)
+
+		then:
+		json.name == 'bpd'
+		json.children[0].name == 'mln'
 	}
 }
