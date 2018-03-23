d3657c3e1c3c5a1802b68e9bb3291ef25900db45
laurie hirsch <lauriehirsch@gmail.com>
2018-03-23 13:29:39 +0000

    Delete TwitterKeys.groovy

--- src/main/groovy/twitter/TwitterKeys.groovy
+++
@@ -1,9 +0,0 @@
-package twitter
-
-class TwitterKeys  {
-
-    final static String consumerKey = "u59ay8TtPUn5p9VTHxdFg"
-    final static String consumerSecret = "LOkS2Vl9KTWXH5VMuwhb9RfIcXBXzkvyzzwD0HQtr14"
-    final static String accessToken = "560297710-4UmsMLOILUgIkLx6V5mdH1lbvG8ew8xvQm5YgBhY"
-    final static String accessTokenSecret = "mlwDtpbx9bUKTTk4wpBYVdUagGmBX6bzAYJbktoNM"
-}
