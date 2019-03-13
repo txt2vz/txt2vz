3b0471d7e2bdad4235535dd70f7a688c956a624d
laurie hirsch <lauriehirsch@gmail.com>
2019-03-13 12:30:02 +0000

    Rineall first files 13 pages only

--- src/main/webapp/extractBOAtext.groovy
+++ src/main/webapp/extractBOAtext.groovy
@@ -2,11 +2,11 @@ import groovy.json.JsonSlurper
 
 def jsonSlurper = new JsonSlurper()
 
-def jfile =  new File('Ranella4363.json')
+def jfile =  new File('Rinella4363first13Pages.json')
 
 def jText = jsonSlurper.parseText(jfile.text)
 
-def outFile = new File ('ranella4363.txt')
+def outFile = new File ('rinella4363first13Pages.txt')
 
 outFile.write(jText.hits.hits.inner_hits.pages.hits.hits._source.text.toString())
 
