2d3b948a7bbe87cb5cb318d130aca1e930467d5d
laurie hirsch <lauriehirsch@gmail.com>
2019-04-02 16:53:48 +0100

    map

--- src/main/groovy/TextDirToJSON.groovy
+++ src/main/groovy/TextDirToJSON.groovy
@@ -1,13 +1,20 @@
 import groovy.io.FileType
 import groovy.json.JsonSlurper
+import net.sf.ehcache.pool.Size
 import org.apache.tika.Tika
 import processText.WordPairsExtractor
 import processText.WordPairsToJSON
 
+
+
 class TextDirToJSON {
 
-    static void main(String[]args){
 
+
+
+
+    static void main(String[]args){
+        def m = ['small': [20,40], 'medium': [40,100], 'large': [200, 400] ]
         final float powerValue = 0.5d
 
         final int maxWordPairs = 40
@@ -23,6 +30,10 @@ class TextDirToJSON {
 //https://stackoverflow.com/questions/7552253/how-to-remove-special-characters-from-a-string
 
         def dir = new File (testDir)//("path_to_parent_dir")
+        m.each{
+            println  "MMMM " + it.value[0] + " " + it.value[1]
+
+        }
         dir.eachFileRecurse (FileType.FILES) { file ->
 
             println "reading file $file"
