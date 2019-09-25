1ee65499965c5adf616aaf5a89d1f8238f29b895
aceslh <lauriehirsch@gmail.com>
2019-09-25 11:33:15 +0100

    swing

--- src/main/groovy/boa/SwingMain.groovy
+++ src/main/groovy/boa/SwingMain.groovy
@@ -3,6 +3,7 @@ package boa
 import groovy.swing.SwingBuilder
 import javax.swing.JFileChooser
 import javax.swing.JLabel
+import javax.swing.JOptionPane
 import static javax.swing.JFrame.EXIT_ON_CLOSE
 import java.awt.*
 
@@ -14,6 +15,7 @@ class SwingMain {
         JLabel textFilePathL;
         File outFile
         File textFile
+       // JOptionPane jp
 
         def initialPath = System.getProperty("user.dir");
         String outFolder
@@ -21,7 +23,7 @@ class SwingMain {
         def swingBuilder = new SwingBuilder()
         swingBuilder.edt {  // edt method makes sure UI is build on Event Dispatch Thread.
             lookAndFeel 'nimbus'  // Simple change in look and feel.
-            frame(title: 'Generate JSON from text', size: [1000, 300],
+            frame(title: 'BOA: Generate JSON from text', size: [1000, 300],
                     show: true, locationRelativeTo: null,
                     defaultCloseOperation: EXIT_ON_CLOSE) {
                 borderLayout(vgap: 5)
@@ -35,11 +37,44 @@ class SwingMain {
 
                         tr {
                             td {
+
+                                button(text: 'Select text file/folder  ',
+                                        toolTipText: 'select the source text file(s)',
+                                        actionPerformed: {
+
+                                            JFileChooser fc = new JFileChooser(initialPath);
+                                            fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+                                            fc.setApproveButtonText('Select File or Folder')
+                                            int result = fc.showOpenDialog(null);
+
+                                            switch (result) {
+                                                case JFileChooser.APPROVE_OPTION:
+
+                                                    textFile = fc.getSelectedFile()
+                                                    textFilePathL.setText(' ' + textFile.toString())
+
+                                                    break;
+                                                case JFileChooser.CANCEL_OPTION:
+                                                case JFileChooser.ERROR_OPTION:
+                                                    break;
+                                            }
+                                        })
+                            }
+                            td {
+
+                                textFilePathL.setText(' ' + initialPath.toString())
+                                label textFilePathL
+                            }
+                        }
+                        tr {
+                            td {
                                 button(text: 'Select folder to save JSON files:',
+                                        toolTipText: 'select folder to store the generated json file(s) ',
                                         actionPerformed: {
 
                                             JFileChooser fc = new JFileChooser(initialPath);
                                             fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                                            fc.setApproveButtonText('Select Folder')
                                             int result = fc.showOpenDialog(null);
                                             switch (result) {
                                                 case JFileChooser.APPROVE_OPTION:
@@ -62,40 +97,20 @@ class SwingMain {
                         }
 
                         tr {
-                            td {
-
-                                button(text: 'Select text file/folder  ',
-                                        actionPerformed: {
-
-                                            JFileChooser fc = new JFileChooser(initialPath);
-                                            fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-                                            int result = fc.showOpenDialog(null);
-
-                                            switch (result) {
-                                                case JFileChooser.APPROVE_OPTION:
-
-                                                    textFile = fc.getSelectedFile()
-                                                    textFilePathL.setText(' ' + textFile.toString())
-                                                    break;
-                                                case JFileChooser.CANCEL_OPTION:
-                                                case JFileChooser.ERROR_OPTION:
-                                                    break;
-                                            }
-                                        })
-                            }
-                            td {
-
-                                textFilePathL.setText(' ' + initialPath.toString())
-                                label textFilePathL
-                            }
-                        }
-                        tr {
 
                             td {
 
                                 label '    '
                             }
 
+                        }
+                        tr {
+                            td {
+                                label (text:'**********************************************************', foreground: Color.BLUE)
+                            }
+                            td {
+                                label (text:'**********************************************************', foreground: Color.BLUE)
+                            }
                         }
 
                         tr {
@@ -115,25 +130,32 @@ class SwingMain {
                                         toolTipText: 'Each text generates its own JSON file',
                                         actionPerformed: {
 
-                                            def genJ = new GenerateJSON(textFile, outFolder)
-                                            genJ.generateMulti()
+                                            if (!textFile.isDirectory())
+                                            {
+                                                JOptionPane.showMessageDialog(null, "Must select text folder for multi option.");
+                                            }
+                                            else {
+
+                                                def genJ = new GenerateJSON(textFile, outFolder)
+                                                genJ.generateMulti()
+                                            }
                                         })
                             }
                         }
                         tr {
                             td {
-                                label '<html>  If selected text source is folder  <br>Files will be merged to one JSON output file </html> '
+                                label '<html>  <br>Multiple files will be merged to one JSON output file </html> '
                             }
                             td {
-                                label '<html>  JSON output file created for <br>Each file found in the source folder </html> '
+                                label '<html>  <br>JSON output file created for each file found in the source folder </html> '
                             }
                         }
                         tr {
                             td {
-                                label '**********************************************************'
+                                label (text:'**********************************************************', foreground: Color.BLUE)
                             }
                             td {
-                                label '**********************************************************'
+                                label (text:'**********************************************************', foreground: Color.BLUE)
                             }
                         }
 
